56cdcd0db8c6d9bf552dcb505c107b12971f1eb9
Matas Petrauskas <ma.petrausk@gmail.com>
2018-03-26 19:51:08 +0300

    Add project files.

---
+++ AudioDeviceSwitcherService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioDeviceSwitcherService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
